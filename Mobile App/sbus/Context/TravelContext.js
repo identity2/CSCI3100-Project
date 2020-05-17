aec5fed39707a9001a27a708f6c12f0b54f1a9a8
RitoGamingPLZ <39838659+RitoGamingPLZ@users.noreply.github.com>
2020-05-17 17:24:51 +0800

    Update TravelContext.js

--- Mobile App/sbus/Context/TravelContext.js
+++ Mobile App/sbus/Context/TravelContext.js
@@ -1,3 +1,12 @@
+/*
+  What: This is this context of navigation data
+  Who: CHIU LIN FUNG 1155109993
+  Where: whole app
+  Why: to remain the state of navigation at the same for all components within navigation page
+  How: simiply call TravelContext and set value when needed in navigation page
+*/
+
+
 import React from 'react';
 
 export const TravelContext = React.createContext(defaultValue);
