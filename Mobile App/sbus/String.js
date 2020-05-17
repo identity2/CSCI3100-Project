6d20a03339af93089246f5a70779e66e4446a5fc
RitoGamingPLZ <39838659+RitoGamingPLZ@users.noreply.github.com>
2020-05-17 17:02:59 +0800

    Update String.js

--- Mobile App/sbus/String.js
+++ Mobile App/sbus/String.js
@@ -1,3 +1,12 @@
+/*
+  What: this is constants file 
+  Who: CHIU LIN FUNG 1155109993
+  Where: mobile application
+  Why: unitize the constant here with better managament
+  How: SERVER_LINK indicate the url of server
+*/
+
+
 const Constants = {
     database_name: "SmartBus.db",
     SERVER_LINK: "http://35.201.158.77:3100/",
