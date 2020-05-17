b48c1fe0221c4524df66d341f26475b334bb0eda
RitoGamingPLZ <39838659+RitoGamingPLZ@users.noreply.github.com>
2020-05-17 17:01:03 +0800

    Update App.js

--- Mobile App/sbus/App.js
+++ Mobile App/sbus/App.js
@@ -1,3 +1,11 @@
+/*
+  What: This is the core part of mobile app which indicate the architecture of whole app
+  Who: CHIU LIN FUNG 1155109993
+  Where: mobile application
+  Why: to construct the architecture of app
+  How: use Screen from js files to construct the layout of App, and also configure route name here
+*/
+
 import React from 'react';
 
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
@@ -13,7 +21,11 @@ import { ScheduleScreen } from './Screens/ScheduleScreen'
 import { TravelScreen } from './Screens/TravelScreen'
 import { SignUpScreen } from './Screens/SignUpScreen'
 
+
+//Color constants
 import COLORS from './Colors';
+
+//Icon library 
 import {Ionicons,  Entypo} from '@expo/vector-icons';
 
 
